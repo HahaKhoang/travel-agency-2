2a0e8b05b612dcf93c5274d27114cb5d1a67c599
Kathy Hoang <kathyhahoang@gmail.com>
2023-08-02 16:42:44 -0500

    Completes WhySection component

--- src/ui/WhySection.jsx
+++ src/ui/WhySection.jsx
@@ -3,6 +3,7 @@ import styles from "./WhySection.module.scss";
 import lofoten from "../../public/img/lofoten.jpg";
 import nz from "../../public/img/nz.jpg";
 import svalbard from "../../public/img/svalbard.jpg";
+import { IoIosStarOutline } from "react-icons/io";
 
 function WhySection() {
   return (
@@ -14,7 +15,44 @@ function WhySection() {
           <img src={nz} className={styles.pic2} />
           <img src={svalbard} className={styles.pic3} />
         </div>
-        <div className={styles.detailsContainer}>Details</div>
+        <div className={styles.detailsContainer}>
+          <div className={styles.details}>
+            <span className={styles.icon}>
+              <IoIosStarOutline />
+            </span>
+            <div className={styles.detailContainer}>
+              <p className={styles.heading}>Heading</p>
+              <p className={styles.detail}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Perferendis dolore, soluta porro dolores
+              </p>
+            </div>
+          </div>
+          <div className={styles.details}>
+            <span className={styles.icon}>
+              <IoIosStarOutline />
+            </span>
+            <div className={styles.detailContainer}>
+              <p className={styles.heading}>Heading</p>
+              <p className={styles.detail}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Perferendis dolore, soluta porro dolores
+              </p>
+            </div>
+          </div>{" "}
+          <div className={styles.details}>
+            <span className={styles.icon}>
+              <IoIosStarOutline />
+            </span>
+            <div className={styles.detailContainer}>
+              <p className={styles.heading}>Heading</p>
+              <p className={styles.detail}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Perferendis dolore, soluta porro dolores
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
