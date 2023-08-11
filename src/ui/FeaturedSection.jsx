d332e710aa8a3af925d70a7fb1be87fd0cafca79
Kathy Hoang <kathyhahoang@gmail.com>
2023-08-11 10:21:31 -0500

    Creates layout for right side of FeaturedSection

--- src/ui/FeaturedSection.jsx
+++ src/ui/FeaturedSection.jsx
@@ -1,5 +1,8 @@
 import styles from "./FeaturedSection.module.scss";
 import kyoto from "../../public/img/kyoto.jpg";
+import Tile from "./Tile";
+import PopularTour from "./PopularTour";
+import { RiVipCrownLine } from "react-icons/ri";
 
 function FeaturedSection() {
   return (
@@ -13,7 +16,25 @@ function FeaturedSection() {
             expedita explicabo incidunt veritatis mollitia?
           </p>
         </div>
-        <div className={styles.tiles}>Create tile component</div>
+        <div className={styles.tiles}>
+          <Tile />
+          <Tile />
+          <Tile />
+          <Tile />
+          <Tile />
+          <Tile />
+        </div>
+      </div>
+      <div className={styles["right-container"]}>
+        <div className={styles["popular-tour"]}>
+          <p>Most popular tours</p>
+          <RiVipCrownLine />
+        </div>
+        <PopularTour />
+        <PopularTour />
+        <PopularTour />
+        <PopularTour />
+        <PopularTour />
       </div>
     </section>
   );
