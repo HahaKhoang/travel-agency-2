740fc53a593d3311f619280ec674876b2a115310
Kathy Hoang <kathyhahoang@gmail.com>
2023-08-16 11:42:15 -0500

    Changes active filter button  with background

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -6,7 +6,7 @@ function Filter({ filterField, options }) {
   const currentFilter = searchParams.get(filterField) || options[0].value;
 
   function handleClick(value) {
-    searchParams.set("type", value);
+    searchParams.set(filterField, value);
     // if (searchParams.get("type")) searchParams.set("type", 1);
     setSearchParams(searchParams);
   }
@@ -15,11 +15,16 @@ function Filter({ filterField, options }) {
     <div className={styles.container}>
       {options.map((option) => (
         <button
-          className={styles.button}
+          className={
+            option.value === currentFilter
+              ? `${styles.button} ${styles.active}`
+              : `${styles.button}`
+          }
           key={option.value}
           onClick={() => handleClick(option.value)}
-          active={option.value === currentFilter}
-          disabled={option.value === currentFilter}
+
+          //   active={option.value === currentFilter}
+          //   disabled={option.value === currentFilter}
         >
           {option.label}
         </button>
