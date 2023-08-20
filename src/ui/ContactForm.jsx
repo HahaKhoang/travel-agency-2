f35cbf9c6080418664a60160d544461b190ea8be
Kathy Hoang <kathyhahoang@gmail.com>
2023-08-20 11:47:51 -0500

    Removes image front Contact page

--- src/ui/ContactForm.jsx
+++ src/ui/ContactForm.jsx
@@ -1,5 +1,4 @@
 import styles from "./ContactForm.module.scss";
-import contact2 from "../../public/img/contact2.jpg";
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
@@ -35,76 +34,68 @@ function ContactForm() {
   }
 
   return (
-    <div
-      className={styles.container}
-      style={{
-        backgroundImage: `url(${contact2})`,
-      }}
-    >
-      <div className={styles.form}>
-        <form ref={form} onSubmit={sendEmail}>
-          <div className={styles["form-row"]}>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={name}
-              required
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-          </div>
-          <div className={styles["form-row"]}>
-            <label htmlFor="email">Email address</label>
-            <input
-              type="text"
-              id="email"
-              name="email"
-              value={email}
-              required
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <div className={styles["form-row"]}>
-            <label htmlFor="reason">What can we help you with:</label>
-            <select
-              name="reason"
-              id="reason"
-              name="select"
-              value={select}
-              onChange={(e) => {
-                console.log(e.target.value);
-                setSelect(e.target.value);
-              }}
-            >
-              <option value="bookTour">I want to book a tour</option>
-              <option value="question">I have a question about a tour</option>
-              <option value="other">Other</option>
-            </select>
-          </div>
-          <div className={styles["form-row"]}>
-            <label htmlFor="freeform">Enter your question here:</label>
-            <textarea
-              id="freeform"
-              rows="7"
-              cols="30"
-              required
-              name="message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-            ></textarea>
-          </div>
-          <div className={styles["form-row"]}>
-            <button type="submit" className={styles.button}>
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
+    <div className={styles.container}>
+      <form ref={form} onSubmit={sendEmail} className={styles.form}>
+        <div className={styles["form-row"]}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            required
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div className={styles["form-row"]}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={email}
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div className={styles["form-row"]}>
+          <label htmlFor="reason">What can we help you with:</label>
+          <select
+            id="reason"
+            name="select"
+            value={select}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setSelect(e.target.value);
+            }}
+          >
+            <option value="bookTour">I want to book a tour</option>
+            <option value="question">I have a question about a tour</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div className={styles["form-row"]}>
+          <label htmlFor="freeform">Enter your question here:</label>
+          <textarea
+            id="freeform"
+            rows="7"
+            cols="30"
+            required
+            name="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          ></textarea>
+        </div>
+        <div className={styles["form-row"]}>
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
