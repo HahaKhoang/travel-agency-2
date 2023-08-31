b9a57d3874db31b15e1d8333ea3df1a0eead3173
Kathy Hoang <kathyhahoang@gmail.com>
2023-08-31 17:00:43 -0500

    Adds underline to active NavBar link

--- src/ui/Banner2.jsx
+++ src/ui/Banner2.jsx
@@ -5,8 +5,8 @@ function Banner2({ h1, text1, text2, svg, bgColor }) {
     <div className={styles.container} style={{ backgroundColor: `${bgColor}` }}>
       <div className={styles["text-container"]}>
         <h1 className={styles.header}>{h1}</h1>
-        <h4 className={styles.text}>{text1}</h4>
-        <h4 className={styles.text}>{text2}</h4>
+        <p className={styles.text}>{text1}</p>
+        <p className={styles.text}>{text2}</p>
       </div>
       <div className={styles["image-container"]}>
         <img src={svg} className={styles.picture} />
--- src/ui/SurpriseBookingHeader.jsx
+++ src/ui/SurpriseBookingHeader.jsx
@@ -3,10 +3,10 @@ import styles from "./SurpriseBookingHeader.module.scss";
 function SurpriseBookingHeader() {
   return (
     <div className={styles.container}>
-      <h3>
+      {/* <h3>
         In order to create a fun surprise for you, we need to know some extra
         details 💕
-      </h3>
+      </h3> */}
 
       <p className={styles.text}>
         Please fill out the form below so we can get more details about your
