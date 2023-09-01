d5df1f596a2ffb8b255d560b76a65e4772f3b259
Kathy Hoang <kathyhahoang@gmail.com>
2023-09-01 09:30:34 -0500

    Connets SurpriseForm to emailJs sans checkbox

--- src/ui/SurpriseForm.jsx
+++ src/ui/SurpriseForm.jsx
@@ -6,8 +6,14 @@ import styles from "./SurpriseForm.module.scss";
 function SurpriseForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const [select, setSelect] = useState("");
-  const [message, setMessage] = useState("");
+  const [duration, setDuration] = useState("");
+  const [budget, setBudget] = useState("");
+  const [people, setPeople] = useState("");
+  const [hobbies, setHobbies] = useState("");
+  const [interests, setInterests] = useState("");
+  const [avoid, setAvoid] = useState("");
+  const [disabilities, setDisabilities] = useState("");
+  const [comments, setComments] = useState("");
 
   const form = useRef();
 
@@ -36,8 +42,8 @@ function SurpriseForm() {
     e.preventDefault();
     emailjs
       .sendForm(
-        "service_nnxnl4p",
-        "template_wrnr298",
+        "service_c07knsu",
+        "template_3zhbdye",
         form.current,
         "M-heCyvRsVkznWfwP"
       )
@@ -51,8 +57,14 @@ function SurpriseForm() {
       );
     setName("");
     setEmail("");
-    setSelect("");
-    setMessage("");
+    setDuration("");
+    setBudget("");
+    setPeople("");
+    setHobbies("");
+    setInterests("");
+    setAvoid("");
+    setDisabilities("");
+    setComments("");
   }
   return (
     <div className={styles.container}>
@@ -101,52 +113,76 @@ function SurpriseForm() {
         </div>
         <div className={styles["form-row"]}>
           <label htmlFor="duration">Duration</label>
-          <select id="duration" name="duration">
-            <option value="1">1 day</option>
-            <option value="2">2 days</option>
-            <option value="3">3 days</option>
-            <option value="4">4 days</option>
-            <option value="5">5 days</option>
-            <option value="6">6 days</option>
-            <option value="7">7 days</option>
-            <option value="8">8 days</option>
-            <option value="9">9 days</option>
-            <option value="10">10 day</option>
-            <option value="11">11 days</option>
-            <option value="12">12 days</option>
-            <option value="13">13 days</option>
-            <option value="14">14 days</option>
+          <select
+            id="duration"
+            name="duration"
+            value={duration}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setDuration(e.target.value);
+            }}
+          >
+            <option value="1 day">1 day</option>
+            <option value="2 days">2 days</option>
+            <option value="3 days">3 days</option>
+            <option value="4 days">4 days</option>
+            <option value="5 days">5 days</option>
+            <option value="6 days">6 days</option>
+            <option value="7 days">7 days</option>
+            <option value="8 days">8 days</option>
+            <option value="9 days">9 days</option>
+            <option value="10 days">10 day</option>
+            <option value="11 days">11 days</option>
+            <option value="12 days">12 days</option>
+            <option value="13 days">13 days</option>
+            <option value="14 days">14 days</option>
             <option value="flexible">I'm flexible</option>
           </select>
         </div>
         <div className={styles["form-row"]}>
           <label htmlFor="budget">Budget per person</label>
-          <select id="budget" name="budget">
-            <option value="1">$500 - $1000</option>
-            <option value="2">$1000 - $1500</option>
-            <option value="3">$1500 - $2000</option>
-            <option value="4">$2000 - $2500</option>
-            <option value="5">$2500 - $3000</option>
-            <option value="6">$3000 - $3500</option>
-            <option value="7">$3500 - $4000</option>
-            <option value="8">$4000 - $4500</option>
-            <option value="9">$4500 - $5000</option>
-            <option value="10">No budget</option>
+          <select
+            id="budget"
+            name="budget"
+            value={budget}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setBudget(e.target.value);
+            }}
+          >
+            <option value="$500 - $1000">$500 - $1000</option>
+            <option value="$1000 - $1500">$1000 - $1500</option>
+            <option value="$1500 - $2000">$1500 - $2000</option>
+            <option value="$2000 - $2500">$2000 - $2500</option>
+            <option value="$2500 - $3000">$2500 - $3000</option>
+            <option value="$3000 - $3500">$3000 - $3500</option>
+            <option value="$3500 - $4000">$3500 - $4000</option>
+            <option value="$4000 - $4500">$4000 - $4500</option>
+            <option value="$4500 - $5000">$4500 - $5000</option>
+            <option value="No budget">No budget</option>
           </select>
         </div>
         <div className={styles["form-row"]}>
           <label htmlFor="people">How many people</label>
-          <select id="people" name="people">
-            <option value="1">1</option>
-            <option value="2">2 </option>
-            <option value="3">3 </option>
-            <option value="4">4 </option>
-            <option value="5">5 </option>
-            <option value="6">6 </option>
-            <option value="7">7 </option>
-            <option value="8">8 </option>
-            <option value="9">9 </option>
-            <option value="10">10 </option>
+          <select
+            id="people"
+            name="people"
+            value={people}
+            onChange={(e) => {
+              console.log(e.target.value);
+              setPeople(e.target.value);
+            }}
+          >
+            <option value="1 person">1</option>
+            <option value="2 people">2 </option>
+            <option value="3 people">3 </option>
+            <option value="4 people">4 </option>
+            <option value="5 people">5 </option>
+            <option value="6 people">6 </option>
+            <option value="7 people">7 </option>
+            <option value="8 people">8 </option>
+            <option value="9 people">9 </option>
+            <option value="10 people">10 </option>
           </select>
         </div>
         <div className={styles["form-row"]}>
@@ -164,9 +200,9 @@ function SurpriseForm() {
             rows="3"
             cols="10"
             required
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            name="hobbies"
+            value={hobbies}
+            onChange={(e) => setHobbies(e.target.value)}
           ></textarea>
         </div>
         <div className={styles["form-row"]}>
@@ -176,9 +212,9 @@ function SurpriseForm() {
             rows="3"
             cols="10"
             required
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            name="interests"
+            value={interests}
+            onChange={(e) => setInterests(e.target.value)}
           ></textarea>
         </div>
         <div className={styles["form-row"]}>
@@ -188,9 +224,9 @@ function SurpriseForm() {
             rows="3"
             cols="10"
             required
-            name="message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            name="avoid"
+            value={avoid}
+            onChange={(e) => setAvoid(e.target.value)}
           ></textarea>
         </div>
         <div className={styles["form-row"]}>
@@ -201,8 +237,9 @@ function SurpriseForm() {
             id="disabilities"
             rows="7"
             cols="10"
-            required
             name="disabilities"
+            value={disabilities}
+            onChange={(e) => setDisabilities(e.target.value)}
           ></textarea>
         </div>
         <div className={styles["form-row"]}>
@@ -213,6 +250,8 @@ function SurpriseForm() {
             cols="10"
             required
             name="comments"
+            value={comments}
+            onChange={(e) => setComments(e.target.value)}
           ></textarea>
         </div>
         <div className={styles["form-row"]}>
