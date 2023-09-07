78b79661279e2e0b5472910a18941378edce1721
Kathy Hoang <kathyhahoang@gmail.com>
2023-09-07 16:23:25 -0500

    Basic styles added to BookingSummary

--- src/ui/BookingSummary.jsx
+++ src/ui/BookingSummary.jsx
@@ -11,7 +11,68 @@ function BookingSummary() {
             <img src={jk2} className={styles.picture} />
           </div>
           <h3>Essential Japan</h3>
-          <p className={styles.text}>Duration: 10 days</p>
+          <div className={styles["content-container"]}>
+            <p className={styles.label}>Duration</p>
+            <p className={styles.content}>10 days</p>
+          </div>
+          <div className={styles["content-container"]}>
+            <p className={styles.label}>Price per person</p>
+            <p className={styles.content}>$50</p>
+          </div>
+          <div className={styles["content-container"]}>
+            <p className={styles.label}>Number of people</p>
+            <p className={styles.content}>3</p>
+          </div>
+          <div className={styles["content-container"]}>
+            <p className={styles.label}>Number of cities</p>
+            <p className={styles.content}>3</p>
+          </div>
+          <div className={styles["extras-container"]}>
+            <p className={styles.label}>Extras</p>
+            <div className={styles.extras}>
+              <p>Accommodation booking service</p>
+              <p>$30</p>
+            </div>
+            <div className={styles.extras}>
+              <p>Flight booking service</p>
+              <p>$90</p>
+            </div>
+          </div>
+          <div className={styles["total-container"]}>
+            <p className={styles.label}>Cost breakdown</p>
+            <div className={styles.extras}>
+              <p>Tour x2</p>
+              <p>$100</p>
+            </div>
+            <div className={styles.extras}>
+              <p>Accommodation booking service</p>
+              <p>$30</p>
+            </div>
+            <div className={styles.extras}>
+              <p>Flight booking service</p>
+              <p>$90</p>
+            </div>
+          </div>
+          <div className={styles.extras}>
+            <p className={styles.total}>Total</p>
+            <p className={styles.price}>$300</p>
+          </div>
+          <p>
+            *Price does not include flight, accommodation, or additional costs.
+            The total cost only covers the itinerary and any additional
+            features.
+          </p>
+          <button className={styles.button}>Book</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookingSummary;
+
+{
+  /* <p className={styles.text}>Duration: 10 days</p>
           <p className={styles.text}>Price: $50</p>
           <p className={styles.text}>Number of people: 2</p>
           <p className={styles.text}>
@@ -20,17 +81,5 @@ function BookingSummary() {
           <p className={styles.text}>Extras: none</p>
           <p className={styles.text}>
             Total:<span className={styles.price}>$300</span>{" "}
-          </p>
-          <p>
-            *Price does not include flight, accommodation, or additional costs.
-            The total cost only covers the itinerary and any additional
-            features.
-          </p>
-          {/* <button className={styles.button}>Book</button> */}
-        </div>
-      </div>
-    </div>
-  );
+          </p> */
 }
-
-export default BookingSummary;
