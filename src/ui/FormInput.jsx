0df5d3adb146df79257a17c4d1fb5993059bf6b0
Kathy Hoang <kathyhahoang@gmail.com>
2023-09-07 11:40:02 -0500

    Adjusts Form gap

--- src/ui/FormInput.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./FormInput.module.scss";
-
-function FormInput({ type, id, name }) {
-  return <input type={type} id={id} name={name} className={styles.input} />;
-}
-
-export default FormInput;
