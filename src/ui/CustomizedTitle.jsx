bbe82b031c9c51e6269c41a65a69dde29ebb0632
Kathy Hoang <kathyhahoang@gmail.com>
2023-12-28 11:22:47 -0600

    Media query for CustomizedTitle up to Tablet

--- src/ui/CustomizedTitle.jsx
+++ src/ui/CustomizedTitle.jsx
@@ -3,10 +3,10 @@ import styles from "./CustomizedTitle.module.scss";
 function CustomizedTitle() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.header}>
+      <p className={styles.header}>
         Tell us about your perfect trip and we'll help you make it come true
-      </h1>
-      <h3 className={styles.text}>Here's how it works</h3>
+      </p>
+      <p className={styles.text}>Here's how it works</p>
     </div>
   );
 }
