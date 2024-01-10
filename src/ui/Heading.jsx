d0670991fb5fbea2c4e9032485adfd318be924a7
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-10 13:29:38 -0600

    Media query for Heading

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -6,7 +6,7 @@ function Heading({ text1, text2, bgColor, fontColor }) {
       className={styles.container}
       style={{ backgroundColor: `${bgColor}`, color: `${fontColor}` }}
     >
-      <h1>{text1}</h1>
+      <h1 className={styles.header}>{text1}</h1>
       <p>{text2}</p>
     </div>
   );
