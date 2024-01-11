81a4a4c242672fb08034499acd02689277ef4e02
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-11 16:02:31 -0600

    Media query for CarouselItem

--- src/ui/CarouselItem.jsx
+++ src/ui/CarouselItem.jsx
@@ -7,14 +7,16 @@ function CarouselItem({ name, tour, image }) {
         <img src={image} className={styles.picture} />
       </div>
       <div className={styles["right-container"]}>
-        <h3 className={styles.tour}>{tour}</h3>
-        <p className={styles.review}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet error
-          fugiat aperiam temporibus non nobis vero earum! Sint beatae facilis
-          perferendis vel voluptas optio aut molestias nostrum dolor, accusamus
-          itaque.
-        </p>
-        <p className={styles.name}>{name}</p>
+        <div className={styles["text-container"]}>
+          <h3 className={styles.tour}>{tour}</h3>
+          <p className={styles.review}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet error
+            fugiat aperiam temporibus non nobis vero earum! Sint beatae facilis
+            perferendis vel voluptas optio aut molestias nostrum dolor,
+            accusamus itaque.
+          </p>
+          <p className={styles.name}>{name}</p>
+        </div>
       </div>
     </div>
   );
