3f4fc192e6f745d4f47106eb396cf90d0b63862b
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-16 14:04:00 -0600

    Media query for CustomizedTitle

--- src/ui/CustomizedTitle.jsx
+++ src/ui/CustomizedTitle.jsx
@@ -3,10 +3,10 @@ import styles from "./CustomizedTitle.module.scss";
 function CustomizedTitle() {
   return (
     <div className={styles.container}>
-      <p className={styles.header}>
+      <h1 className={styles.header}>
         Tell us about your perfect trip and we'll help you make it come true
-      </p>
-      <p className={styles.text}>Here's how it works</p>
+      </h1>
+      <h3 className={styles.text}>Here's how it works</h3>
     </div>
   );
 }
