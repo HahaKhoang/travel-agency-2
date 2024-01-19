1fda1e7c7b32d8d58d463d73974b16fe7e492138
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-19 11:24:39 -0600

    Media query for SingleTourBookNow

--- src/ui/SingleTourBookNow.jsx
+++ src/ui/SingleTourBookNow.jsx
@@ -14,9 +14,9 @@ function SingleTourBookNow({ duration, price, cities, category, slug }) {
     <div className={styles.container}>
       <div className={styles["details-container"]}>
         <div className={styles["details-center"]}>
-          <p className={styles.header}>
+          <h3 className={styles.header}>
             {duration} {day} from ${price}~
-          </p>
+          </h3>
           <div className={styles.tour}>
             <p className={styles.label}>
               Category: {category.charAt(0).toUpperCase() + category.slice(1)}
