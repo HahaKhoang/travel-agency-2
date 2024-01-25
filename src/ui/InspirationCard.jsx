1193ddc28bd054fd15b37748ef78618660d493e4
Kathy Hoang <kathyhahoang@gmail.com>
2024-01-25 11:33:49 -0600

    Media query for InspirationCard

--- src/ui/InspirationCard.jsx
+++ src/ui/InspirationCard.jsx
@@ -7,19 +7,19 @@ function TourCard({ name, location, img, slug }) {
     <>
       <div className={styles.container}>
         <div className={styles["image-container"]}>
-          <div className={styles.info}>
+          <div className={`${styles.info} ${styles.hidden}`}>
             <div className={styles.content}>
               <p className={styles.title}>{name}</p>
+              <div className={styles.location}>
+                <RiMapPin2Line className={styles.icon} />
+                <p>{location}</p>
+              </div>
               <NavLink to={`/all-tours/${slug}`} className={styles.button}>
                 Read more
               </NavLink>
             </div>
           </div>
           <img src={img} className={styles.image} />
-          <div className={styles.location}>
-            <RiMapPin2Line className={styles.icon} />
-            <p>{location}</p>
-          </div>
         </div>
       </div>
     </>
