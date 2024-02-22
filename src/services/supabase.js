ced8f8f98ce6b86454975cf95b4b54f7d0621e2c
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-21 21:29:52 -0600

    Updates supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASEURL;
-const supabaseKey = import.meta.env.VITE_SUPABASEKEY;
+const supabaseUrl = "https://lhxnyafaldsdteqwknyq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxoeG55YWZhbGRzZHRlcXdrbnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIxMjAxMzcsImV4cCI6MjAwNzY5NjEzN30.VaqqiJNi8PYxBZgOrRGKeYl256j0F9IB5byCxYb3nBQ";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
