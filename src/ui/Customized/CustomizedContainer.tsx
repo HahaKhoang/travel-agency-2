cd58e2f7fc1adf92ab0a6e04333659065168d46e
Kathy Hoang <kathyhahoang@gmail.com>
2024-02-29 10:33:05 -0600

    Condenses CustomizedSteps into one component

--- src/ui/Customized/CustomizedContainer.tsx
+++ src/ui/Customized/CustomizedContainer.tsx
@@ -3,15 +3,15 @@ import CustomizedStep1 from "./CustomizedStep1";
 import CustomizedStep3 from "./CustomizedStep3";
 import CustomizedCTA from "./CustomizedCTA";
 import CustomizedTitle from "./CustomizedTitle";
+import CustomizedSteps from "../../components/CustomizedSteps";
 
 function CustomizedContainer() {
   return (
     <div>
       {/* <CustomizedHeader />
-      <CustomizedTitle />
-      <CustomizedStep1 />
-      <CustomizedStep3 />
-      <CustomizedCTA /> */}
+      <CustomizedTitle /> */}
+      <CustomizedSteps />
+      {/* <CustomizedCTA /> */}
     </div>
   );
 }
