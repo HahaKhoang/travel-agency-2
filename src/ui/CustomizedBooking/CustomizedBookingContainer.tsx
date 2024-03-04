1ce005caa89af4de4a1112a36c113727f993488b
Kathy Hoang <kathyhahoang@gmail.com>
2024-03-04 14:46:26 -0600

    Updates CustomizedBookingSummary

--- src/ui/CustomizedBooking/CustomizedBookingContainer.tsx
+++ src/ui/CustomizedBooking/CustomizedBookingContainer.tsx
@@ -2,16 +2,23 @@ import CustomizedBookingForm from "./CustomizedBookingForm.tsx";
 import styles from "./CustomizedBookingContainer.module.scss";
 import customized from "../../assets/img/website/customized.jpg";
 import TripSummary from "../TripSummary.tsx";
+import CustomizedBookingSummary from "./CustomizedBookingSummary.tsx";
 
 function CustomizedBookingContainer() {
   return (
     <div style={{ backgroundColor: "var(--color-blue-dark" }}>
       <div className={styles.container}>
         <CustomizedBookingForm />
-        <TripSummary
+        {/* <TripSummary
           image={customized}
           header="Customized tour"
           slice="customized"
+          disclaimer="*Price does not include flight, accommodation, or additional costs.
+          The total cost only covers the itinerary and any additional features."
+        /> */}
+        <CustomizedBookingSummary
+          image={customized}
+          header="Customized tour"
           disclaimer="*Price does not include flight, accommodation, or additional costs.
           The total cost only covers the itinerary and any additional features."
         />
--- src/ui/CustomizedBooking/CustomizedBookingSummary.tsx
+++ src/ui/CustomizedBooking/CustomizedBookingSummary.tsx
@@ -1,22 +1,20 @@
 import styles from "./CustomizedBookingSummary.module.scss";
 import customized from "../../public/img/website/customized.jpg";
-import { useSelector } from "react-redux";
+import { useAppSelector } from "../../store/hooks";
 
-function CustomizedBookingSummary() {
-  const duration = useSelector((state) => state.customized.duration);
-  const price = useSelector((state) => state.customized.price);
-  const quantity = useSelector((state) => state.customized.quantity);
+function CustomizedBookingSummary({ image, header, disclaimer }) {
+  const {
+    duration,
+    quantity,
+    price,
+    accommodationFee,
+    flightFee,
+    reservationFee,
+  } = useAppSelector((state) => state.customized);
+
   const day = duration < 2 ? "day" : "days";
   const durationField =
     duration === "flexible" ? "I'm flexible" : `${duration} ${day}`;
-
-  const accommodationFee = useSelector(
-    (state) => state.customized.accommodationFee
-  );
-  const flightFee = useSelector((state) => state.customized.flightFee);
-  const reservationFee = useSelector(
-    (state) => state.customized.reservationFee
-  );
 
   const tourTotal = price * quantity;
   const totalPrice = accommodationFee ? tourTotal + 90 : tourTotal;
@@ -26,93 +24,60 @@ function CustomizedBookingSummary() {
   return (
     <div className={styles.container}>
       <div className={styles.box}>
-        <div className={styles["info-container"]}>
-          <h3>Trip summary</h3>
-          <div className={styles["picture-container"]}>
-            <img src={customized} className={styles.picture} />
-          </div>
-          <h3>Customized tour</h3>
-          <div className={styles["content-container"]}>
-            <p className={styles.label}>Duration</p>
-            <p className={styles.content}>{durationField}</p>
-          </div>
-          <div className={styles["content-container"]}>
-            <p className={styles.label}>Price per person</p>
+        <div className={styles["picture-container"]}>
+          <img src={image} className={styles.picture} />
+        </div>
+
+        <h3>{header ? `${header}` : `${name}`}</h3>
+        <div className={styles["details-container"]}>
+          <div className={styles.details}>
+            <p>Price</p>
             <p className={styles.content}>${price}</p>
           </div>
-          <div className={styles["content-container"]}>
-            <p className={styles.label}>Number of people</p>
+          <div className={styles.details}>
+            <p>Number of people</p>
             <p className={styles.content}>{quantity}</p>
           </div>
-
-          <div className={styles["extras-container"]}>
-            <p className={styles.label}>Extras</p>
-            {!accommodationFee && !flightFee && !reservationFee && (
-              <div className={styles.extras}>
-                <p>None</p>
-              </div>
-            )}
-            {accommodationFee && (
-              <div className={styles.extras}>
-                <p>Accommodation booking service</p>
-                <p>$90</p>
-              </div>
-            )}
-            {flightFee && (
-              <div className={styles.extras}>
-                <p>Flight booking service</p>
-                <p>$140</p>
-              </div>
-            )}
-            {reservationFee && (
-              <div className={styles.extras}>
-                <p>Reservation booking service</p>
-                <p>$130</p>
-              </div>
-            )}
+          <div className={styles.details}>
+            <p>Duration</p>
+            <p className={styles.content}>
+              {duration} {day}
+            </p>
           </div>
-          <div className={styles["total-container"]}>
-            <p className={styles.label}>Cost breakdown</p>
-            <div className={styles.extras}>
-              <p>Tour x{quantity}</p>
-              <p>${tourTotal}</p>
-            </div>
-            {accommodationFee && (
-              <div className={styles.extras}>
-                <p>Accommodation booking service</p>
-                <p>$90</p>
-              </div>
-            )}
-            {flightFee && (
-              <div className={styles.extras}>
-                <p>Flight booking service</p>
-                <p>$140</p>
-              </div>
-            )}
-            {reservationFee && (
-              <div className={styles.extras}>
-                <p>Reservation booking service</p>
-                <p>$130</p>
-              </div>
-            )}
-          </div>
-          <div className={styles.extras}>
-            <p className={styles.total}>Total</p>
-            <p className={styles.price}>${totalPrice3}</p>
-          </div>
-          <p>
-            *Price does not include flight, accommodation, or additional costs.
-            The total cost only covers the itinerary and any additional
-            features.
-          </p>
-          <button
-            className={styles.button}
-            type="submit"
-            form="customized-form"
-          >
-            Book
-          </button>
         </div>
+        <div className={styles["price-details"]}>
+          <p className={styles.price}>Price details</p>
+          <div className={styles.breakdown}>
+            <p>Tour x{quantity}</p>
+            <p className={styles.content}>${tourTotal}</p>
+          </div>
+          {accommodationFee && (
+            <div className={styles.breakdown}>
+              <p>Accommodation booking service</p>
+              <p className={styles.content}>$90</p>
+            </div>
+          )}
+          {flightFee && (
+            <div className={styles.breakdown}>
+              <p>Flight booking service</p>
+              <p className={styles.content}>$140</p>
+            </div>
+          )}
+          {reservationFee && (
+            <div className={styles.breakdown}>
+              <p>Reservation booking service</p>
+              <p className={styles.content}>$130</p>
+            </div>
+          )}
+
+          <div className={styles["total-container"]}>
+            <div className={styles.total}>
+              <p>Total</p>
+              <p>${totalPrice3}</p>
+            </div>
+          </div>
+        </div>
+        <p className={styles.disclaimer}>{disclaimer}</p>
       </div>
     </div>
   );
